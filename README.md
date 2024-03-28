401e817cc87cf6e73ae1bc39a9cbd643af9302c5
Martin Künzel <mlk.nrw.de@googlemail.com>
2024-03-28 18:04:59 +0100

    add sources for reshade, cleanup code

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # What does the script do
 
-The script loads (from a config file) entries for git repositories that supply reShade filters and tries to add them to a merged dirstructure and then to a vkBasalt.conf
+The script loads (from a config file sources_reshade.conf) entries for git repositories that supply reShade filters and tries to add them to a merged dirstructure and then to a vkBasalt.conf
 
 The related parameters of all found filters will try to be added to the vkBasalt.conf as well (duplicate filter names could pose problems. see [https://github.com/DadSchoorse/vkBasalt/pull/46#issuecomment-570783537](link))
+
+the vkBasalt.conf will contain a prefilled head part that can be defined in vkBasalt.conf.default.head
 
 # how to start
 `./run.sh`
--- run.sh
+++ run.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 IFS=$'\n';
 
-PATH_SCRIPT=$(realpath $(dirname ${BASH_SOURCE[0]}));
-FILE_DEFAULT_VKBASALT=${PATH_SCRIPT}/vkBasalt.conf.default
+PATH_SCRIPT="$(realpath $(dirname ${BASH_SOURCE[0]}))";
+FILE_DEFAULT_VKBASALT="${PATH_SCRIPT}/vkBasalt.conf.default.head"
 
-PATH_BUILD=${PATH_SCRIPT}/build
-PATH_SHADERS=${PATH_BUILD}/shaders
+PATH_BUILD="${PATH_SCRIPT}/build"
+PATH_SHADERS="${PATH_BUILD}/shaders"
 
-FILE_VKBASALT=${PATH_BUILD}/vkBasalt.conf
-
-echo "cleaning build path" && rm "${PATH_BUILD}/*" -rf;
-
-repos_shaders=("https://github.com/CeeJayDK/SweetFX sweetfx-shaders" "https://github.com/martymcmodding/qUINT martymc-shaders" "https://github.com/BlueSkyDefender/AstrayFX astrayfx-shaders" "https://github.com/prod80/prod80-ReShade-Repository prod80-shaders" "https://github.com/crosire/reshade-shaders reshade-shaders_slim"); 
+FILE_VKBASALT="${PATH_BUILD}/vkBasalt.conf"
 
 fn_exit_err() { echo "$2"; exit $1; }
 fn_exit() { echo exiting...; echo done; }
@@ -19,13 +15,18 @@ trap fn_exit EXIT;
 
 command -v git >/dev/null || fn_exit_err $? "could not find git";
 
+echo "cleaning build path";
+find ${PATH_BUILD} -mindepth 1 -maxdepth 1 -type d -execdir rm -rf {} \; ;
+rm ${FILE_VKBASALT};
+
 fn_git_load_shaders() { 
 	mkdir -p ${PATH_SHADERS} || fn_exit_err $? "could not create shaders basedir";
 	cd ${PATH_SHADERS} || fn_exit_err $? "could not open shaders basedir";
-	for i in ${repos_shaders[@]}; do 
-		git clone $(printf $i|awk '{print $1}'); 
+	for i in $(grep -E '^http.*' ${PATH_SCRIPT}/sources_reshade.conf|sed 's/\s//g'); do
+		git clone ${i} #|| exit_err $? "Error while trying to fetch shaders from ${i}";
 	done;
 }
+
 fn_link_shaders() {
 	cd ${PATH_SHADERS} || fn_exit_err $? "could not open shaders basedir @${PATH_SHADERS}";
 	mkdir -p Merged/{Textures,Shaders} || fn_exit_err $? "could not create dirstructure for merged shaders";
@@ -35,7 +36,7 @@ fn_link_shaders() {
 	cd Merged/Shaders;
 	find ../.. -wholename '*/*haders/*.fx*' -type f -exec ln -fs {} ./ \; ;
 	cd -;
-};
+}
 
 fn_create_conf_vkbasalt() {
 	(
